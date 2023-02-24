dc1d385b1c5781cc8a9f408e2f7af8f2ca952f86
Chunkai <chunkai369@gmail.com>
2023-02-24 23:24:18 +0000

    test helper

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,4 +1,5 @@
-require "../test_helper"
+$LOAD_PATH.unshift File.expand_path("../test", __dir__)
+require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
